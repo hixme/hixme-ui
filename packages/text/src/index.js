b6c377c289bdd126ea3785ffd0fcecbd7194fc55
Billy Montgomery <billy.montgomery@gmail.com>
2017-11-24 01:44:27 -0800

    feature/text: add color, font size, and text alignment props handling

--- packages/text/src/index.js
+++ packages/text/src/index.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
+import themeProps, { getTextColor, getFontSize } from '@hixme-ui/theme-props'
 
 const Text = styled.span`
+  color: ${props => getTextColor(props)};
+  font-size: ${props => props.fontSize || getFontSize(props)};
+  text-align: ${props => {
+    if (props.left) return 'left'
+    if (props.center) return 'center'
+    if (props.right) return 'right'
 
+    return 'inherit'
+  }};
 `
 
 Text.displayName = 'hui:Text'
