f1af9a51243a89bd688e0c5c6e038c281773d11d
Billy Montgomery <billy.montgomery@gmail.com>
2017-11-24 22:28:49 -0800

    feature/text: add Text route to hui-pages

--- packages/hui-pages/src/routes/index.js
+++ packages/hui-pages/src/routes/index.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home';
 import FormGroupDocs from './FormGroupDocs';
+import TextDocs from './TextDocs';
 import NotFound from './NotFound';
 
 const Routes = (props) => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/form-group" component={FormGroupDocs} />
+    <Route path="/text" component={TextDocs} />
     <Route component={NotFound} />
   </Switch>
 );
