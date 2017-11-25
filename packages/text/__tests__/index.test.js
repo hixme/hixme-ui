fc8fbe4f71078aac22dedbcfa9e2e5a3a2d94929
Billy Montgomery <billy.montgomery@gmail.com>
2017-11-24 22:29:30 -0800

    feature/text: add ThemeProvider with test

--- packages/text/__tests__/index.test.js
+++ packages/text/__tests__/index.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { ThemeProvider } from '@hixme-ui/theme'
 import Text from '../src/index.js'
 
-test(`${Text.displayName}`, () => {
+test('Input', () => {
   const component = renderer.create(
-    (<Text>I said good day sir</Text>),
+    (<ThemeProvider><Text>I said good day sir</Text></ThemeProvider>),
   )
 
   const tree = component.toJSON()
