8502f07686ddf717c4ed5b15e4cd48242cd5d440
Billy Montgomery <billy.montgomery@gmail.com>
2017-11-25 20:28:32 -0800

    feature/text: export Text default and named Tag options

---
+++ packages/text/src/index.js
@@ -0,0 +1,11 @@
+import Text, {
+  Anchor as _Anchor,
+  Paragraph as _Paragraph,
+  Label as _Label,
+} from './Text'
+
+export const Anchor = _Anchor
+export const Paragraph = _Paragraph
+export const Label = _Label
+
+export default Text
