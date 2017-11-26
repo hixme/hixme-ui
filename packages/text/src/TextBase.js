2994e4df8c49b7a6ad979dc1af38ffac3493fdcd
Billy Montgomery <billy.montgomery@gmail.com>
2017-11-25 21:07:55 -0800

    feature/text: add textIndent, textOverflow, userSelect, wordBreak, and cursor helper props

--- packages/text/src/TextBase.js
+++ packages/text/src/TextBase.js
@@ -13,7 +13,11 @@ const TextBase = styled.span`
     if (props.dashed) return '1px dashed'
     return 'none'
   }};
-  cursor: ${props => props.cursor || 'auto'};
+  cursor: ${(props) => {
+    if (props.cursor) return props.cursor
+    if (props.a) return 'pointer'
+    return 'auto'
+  }};
   display: ${props => props.display || 'inline-flex'};
   justify-content: ${(props) => {
     if (props.right) return 'flex-end'
@@ -28,15 +32,27 @@ const TextBase = styled.span`
 
     return 'inherit'
   }};
+  text-indent: ${props => props.textIndent || 'inherit'};
   text-transform: ${(props) => {
     if (props.uppercase) return 'uppercase'
     if (props.lowercase) return 'lowercase'
     if (props.capitalize) return 'capitalize'
     return 'none'
   }};
+  text-overflow: ${props => props.textOverflow || 'inherit'};
+  user-select: ${(props) => {
+    if (props.userSelect) return props.userSelect
+    if (props.label) return 'none'
+    return 'inherit'
+  }};
+  white-space: ${props => props.whiteSpace || 'inherit'};
   width: ${(props) => {
     if (props.right || props.left || props.center) return '100%'
     return 'auto'
+  }};
+  word-break: ${(props) => {
+    if (props.wordBreak) return props.wordBreak
+    return 'inherit'
   }};
 `
 
