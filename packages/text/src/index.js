ec2f6871bf46c1dfab3724dfdf239a79ab73bd23
Billy Montgomery <billy.montgomery@gmail.com>
2017-12-01 22:43:04 -0800

    feature/title: expose TextBase from text

--- packages/text/src/index.js
+++ packages/text/src/index.js
@@ -3,7 +3,9 @@ import Text, {
   Paragraph as _Paragraph,
   Label as _Label,
 } from './Text'
+import _TextBase from './TextBase'
 
+export const TextBase = _TextBase
 export const TextLink = _TextLink
 export const Paragraph = _Paragraph
 export const Label = _Label
