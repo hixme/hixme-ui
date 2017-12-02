36ebdb435588da88ca7962f9eeddaf3342591a2c
Calvin Cieslak <calvin.cieslak@gmail.com>
2017-12-02 11:05:53 -0800

    feature/content-container: Add docs for ContentContainer

---
+++ packages/hui-pages/src/routes/ContentContainerDocs/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Text from '@hixme-ui/text'
+import Title from '@hixme-ui/title'
+
+const ContentContainerDocs = () => (
+  <div>
+    <Title>ContentContainer</Title>
+    <Text>A container component that imposes a max width on its contents</Text>
+    {`<ContentContainer>
+  <h1>Something very wide</h1>
+  <p>
+    Lorem ipsum...
+  </p>
+</ContentContainer>
+`}
+  </div>
+)
+
+export default ContentContainerDocs
+
