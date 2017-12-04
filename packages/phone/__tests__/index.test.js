49725a9f1ef9128ca893c257466023fc2a6b70ad
Billy Montgomery <billy.montgomery@gmail.com>
2017-12-03 23:08:44 -0800

    feature/phone: render Text component with phone format. Add tests

--- packages/phone/__tests__/index.test.js
+++ packages/phone/__tests__/index.test.js
@@ -4,12 +4,22 @@ import { ThemeProvider } from '@hixme-ui/theme'
 import Phone from '../src/index.js'
 
 describe('Phone', () => {
-  it('Should be an a tag', () => {
-    const component = renderer.create(
+  const component = renderer.create(
       (<ThemeProvider><Phone number='8881234567' /></ThemeProvider>),
     )
 
-    const tree = component.toJSON()
+  const tree = component.toJSON()
+  console.log(tree)
+  it('Should be an a tag', () => {
     expect(tree.type).toBe('a')
+  })
+
+  it('Should have an href of tel:8881234567', () => {
+    expect(tree.props.href).toBeDefined()
+    expect(tree.props.href).toBe('tel:8881234567')
+  })
+
+  it('Should render children with phone number', () => {
+    expect(tree.children[0]).toBe('(888) 123 - 4567')
   })
 })
--- packages/phone/src/Phone.js
+++ packages/phone/src/Phone.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TextBase } from '@hixme-ui/text'
+import Text from '@hixme-ui/text'
+
+function getNumbers(str) {
+  return str.replace(/[^0-9.]/g, '')
+}
 
 const Phone = ({
- children,
-}) => children
+  number,
+  ...props
+}) => <Text href={`tel:${getNumbers(number)}`} {...props}>{Phone.format(number)}</Text>
+
+Phone.format = function formatPhoneNumber(str) {
+  const number = getNumbers(str)
+  return `(${number.substring(0, 3)}) ${number.substring(3, 6)} - ${number.substring(6, 10)}`
+}
 
 Phone.displayName = 'hui:Phone'
 Phone.huiName = 'Phone'
 
 Phone.propTypes = {
-  children: PropTypes.any,
+  number: PropTypes.string.isRequired,
 }
+
 Phone.defaultProps = {
-  children: null,
+  a: true,
 }
 
 export default Phone
