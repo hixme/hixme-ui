ecdb6bfc2b674de7db3b5bd42f68c049bd913ca8
Calvin Cieslak <calvin.cieslak@gmail.com>
2017-12-29 16:34:21 -0800

    feature/price: Update Price component

--- packages/price/src/Price.js
+++ packages/price/src/Price.js
@@ -5,90 +5,55 @@ import PropTypes from 'prop-types'
 import Container from '@hixme-ui/container'
 import Text from '@hixme-ui/text'
 
-import { fontFamily, colorTheme } from '@hixme-ui/theme'
+import StyledDollarSign from './StyledDollarSign'
+import StyledDollars from './StyledDollars'
+import StyledCents from './StyledCents'
 
-class Price extends Text {
-  static displayName = 'hui:Price'
-  static huiName = 'Price'
+const Price = ({
+  label,
+  value,
+  ...restProps
+}) => {
+  const getValue = num => ({
+    dollars: parseFloat(num).toFixed(2).split('.')[0],
+    cents: parseFloat(num).toFixed(2).split('.')[1],
+  })
 
-  getProps() {
-    const {
-      value,
-      label,
-    } = this.props
+  const getColor = val => (val < 0)
 
-    return Object.assign({}, super.getProps(), {
-      value,
-      label,
-    })
-  }
-
-  getValue() {
-    return {
-      dollars: parseFloat(this.props.value).toFixed(2).split('.')[0],
-      cents: parseFloat(this.props.value).toFixed(2).split('.')[1],
-    }
-  }
-
-  render() {
-    const dollarSignProps = {
-      style: Object.assign(
-        {},
-        this.getStyles(),
-        {
-          fontSize: '15pt',
-          padding: '0 5px 0 0',
-          fontFamily: fontFamily.thin,
-        },
-        ((this.getProps().value < 0) ? { color: colorTheme.blue } : {}),
-      ),
-    }
-    const dollarProps = {
-      style: Object.assign(
-        {},
-        this.getStyles(),
-        {
-          margin: '-10px 0 0 0',
-          fontSize: '32pt',
-          fontFamily: fontFamily.bolder,
-        },
-        ((this.getProps().value < 0) ? { color: colorTheme.blue } : {}),
-      ),
-    }
-    const centProps = {
-      style: Object.assign(
-        {},
-        this.getStyles(),
-        {
-          fontSize: '13pt',
-          fontFamily: fontFamily.bolder,
-        },
-        ((this.getProps().value < 0) ? { color: colorTheme.blue } : {}),
-      ),
-    }
-
-    return (
-      <Container flex alignStart>
-        <Container flex alignCenter flexColumn margin='5px 0'>
-          <Container noPadding>
-            <span id='dollar-sign' {...dollarSignProps}>$</span>
-            <span id='dollars' {...dollarProps}>{this.getValue().dollars}</span>
-            <span id='cents' {...centProps}>.{this.getValue().cents}</span>
-          </Container>
-          {this.props.label !== '' &&
-            <Container margin='-10px 0 0 0'>
-              <Text id='label' fontSize='8pt' light>{this.props.label}</Text>
-            </Container>
-          }
+  return (
+    <Container flex alignStart noPadding>
+      <Container flex alignCenter flexColumn margin='5px 0' noPadding>
+        <Container flex noPadding>
+          <StyledDollarSign id='dollar-sign' blue={getColor(value)} {...restProps}>$</StyledDollarSign>
+          <StyledDollars id='dollars' blue={getColor(value)} {...restProps}>
+            {getValue(value).dollars}
+          </StyledDollars>
+          <StyledCents id='cents' blue={getColor(value)} {...restProps}>
+            .
+            {getValue(value).cents}
+          </StyledCents>
         </Container>
+        {label !== '' &&
+          <Container margin='-10px 0 0 0' noPadding>
+            <Text id='label' fontSize='8pt' light>{label}</Text>
+          </Container>
+        }
       </Container>
-    )
-  }
+    </Container>
+  )
 }
 
 Price.propTypes = {
   value: PropTypes.number.isRequired,
   label: PropTypes.string,
 }
+
+Price.defaultProps = {
+  label: '',
+}
+
+Price.displayName = 'hui:Price'
+Price.huiName = 'Price'
 
 export default Price
