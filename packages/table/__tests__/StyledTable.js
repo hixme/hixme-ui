9ff12da12664e050e5bec8f81fcb84a733eab6b0
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-01-08 13:28:12 -0500

    improvement/table-tests: Add some table tests

---
+++ packages/table/__tests__/StyledTable.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { ThemeProvider } from '@hixme-ui/theme'
+import 'jest-styled-components'
+
+import StyledTable from '../src/StyledTable'
+
+describe('StyledTable', () => {
+  test('Should render ok', () => {
+    const component = renderer.create(
+      <ThemeProvider>
+        <StyledTable />
+      </ThemeProvider>
+    )
+
+    const tree = component.toJSON()
+    expect(tree.type).toBe('table')
+  })
+
+  test('Should have all necessary global styles', () => {
+    const component = renderer.create(
+      <ThemeProvider>
+        <StyledTable />
+      </ThemeProvider>,
+    )
+
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('width', '100%')
+    expect(tree).toHaveStyleRule('border-collapse', 'collapse')
+    expect(tree).toHaveStyleRule('min-width', '350px')
+    expect(tree).toHaveStyleRule('font-family', '\'Avenir-Next_Demi\',\'HelveticaNeue-Medium\',\'Helvetica Neue Medium\',\'Helvetica Neue\',Helvetica,Arial,\'Lucida Grande\',sans-serif')
+  })
+})
+
