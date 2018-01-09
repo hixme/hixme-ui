05a3f9b7a615b39b9b1c56c55137bf4ad982c09d
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-09 13:07:16 -0800

    improvement/more-docs: add github component link to docs titles

---
+++ packages/hui-pages/src/components/GithubComponentLink/GithubLink.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import Text from '@hixme-ui/text'
+
+import { componentURL } from '../../modules/github-utils'
+
+const GithubLink = withRouter(({ location }) => (
+  <Text href={componentURL(location.pathname.replace('/', ''))} cursor='pointer'>
+    View on Github
+  </Text>
+))
+
+GithubLink.displayName = 'GithubLink'
+
+export default GithubLink
---
+++ packages/hui-pages/src/modules/github-utils/index.js
@@ -0,0 +1,5 @@
+export const repositoryURL = 'https://github.com/hixme/hixme-ui'
+
+export function componentURL(component) {
+  return `${repositoryURL}/tree/master/packages/${component}`
+}
