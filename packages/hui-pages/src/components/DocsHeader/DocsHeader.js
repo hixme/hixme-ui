138df32f8d572132d5709254fe2534b97498226b
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-09 19:24:45 -0800

    fix/hui-back-home: use react router Link to go back to root

--- packages/hui-pages/src/components/DocsHeader/DocsHeader.js
+++ packages/hui-pages/src/components/DocsHeader/DocsHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid'
+import { Link } from 'react-router-dom'
 
 import Text from '@hixme-ui/text'
 
@@ -7,8 +8,10 @@ const DocsHeader = () => (
   <Grid fluid style={{ padding: '0' }}>
     <Row style={{ margin: '20px 0 30px 0' }}>
       <Col>
-        <Text a light href='/' cursor='pointer'>
-          {'< Back to Home'}
+        <Text light>
+          <Link to='/'>
+            {'< Back to Home'}
+          </Link>
         </Text>
       </Col>
     </Row>
