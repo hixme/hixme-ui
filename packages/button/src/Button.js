b1ee3f4a9b86ba710bb0fc44a929f0f1978fd8d0
willthebrit <will.morgan@hixme.com>
2018-01-16 14:19:39 -0800

    improvement/submit-button: Make any button a submit button!

--- packages/button/src/Button.js
+++ packages/button/src/Button.js
@@ -1,27 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import OutlineButton from './OutlineButton'
 import GradientButton from './GradientButton'
 
 const Button = ({
+  children,
+  disabled,
   outline,
+  submitting,
+  submittingText,
   ...props
 }) => {
   if (outline) {
-    return (<OutlineButton {...props} />)
+    return (
+      <OutlineButton
+        {...props}
+        disabled={submitting || disabled}
+      >
+        {submitting ? submittingText : children}
+      </OutlineButton>
+    )
   }
 
-  return (<GradientButton {...props} />)
+  return (
+    <GradientButton
+      {...props}
+      disabled={submitting || disabled}
+    >
+      {submitting ? submittingText : children}
+    </GradientButton>
+  )
 }
 
 Button.displayName = 'hui:Button'
 Button.huiName = 'Button'
 
 Button.propTypes = {
+  disabled: PropTypes.bool,
+  children: PropTypes.node,
   outline: PropTypes.bool,
+  submitting: PropTypes.bool,
+  submittingText: PropTypes.string,
 }
+
 Button.defaultProps = {
+  disabled: false,
+  children: null,
   outline: false,
+  submitting: false,
+  submittingText: 'Submitting...',
 }
 
 export default Button
