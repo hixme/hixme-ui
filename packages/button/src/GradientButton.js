c23ab6e240232b9791f5ad775981dfda0452f96c
willthebrit <will.morgan@hixme.com>
2018-01-16 15:22:50 -0800

    improvement/submit-button: Add ButtonBase, cursor pointer

--- packages/button/src/GradientButton.js
+++ packages/button/src/GradientButton.js
@@ -1,46 +1,18 @@
-import styled from 'styled-components'
 import {
   getButtonGradientColor,
-  getButtonTextColor,
   getButtonBorderRadius,
-  getButtonHeight,
-  getButtonMinWidth,
-  getButtonFontSize,
-  getShadow,
  } from '@hixme-ui/theme-props'
 
-const GradientButton = styled.button`
-  transition: all .25s ease;
-  :hover:enabled {
-    background-size: 100%;
-  };
-  :active:enabled {
-    box-shadow: ${props => getShadow(props, 'none')};
-  };
-  :disabled {
-    opacity: .4;
-    cursor: not-allowed;
-  };
-  height: ${props => getButtonHeight(props)};
-  min-width: ${props => getButtonMinWidth(props)};
-  max-width: ${props => (props.maxWidth ? props.maxWidth : 'none')};
-  width: ${(props) => {
-    if (props.width) return props.width
-    if (props.block) return '100%'
-    return 'auto'
-  }};
-  font-size:${props => getButtonFontSize(props)};
-  color: ${props => getButtonTextColor(props)};
+import ButtonBase from './ButtonBase'
+
+const GradientButton = ButtonBase.extend`
   background: ${props => getButtonGradientColor(props)};
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
   background-size: 200%;
   border-style: none;
-  outline: none;
-  cursor: pointer;
-  padding: 0.25em 1em;
   border-radius: ${props => getButtonBorderRadius(props)};
+  * {
+    cursor: pointer;
+  }
 `
 
 GradientButton.displayName = 'hui:GradientButton'
