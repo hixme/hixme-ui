7bc763f7d61950d5d507eb15a9dbe0d8138cfb4c
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-01-18 15:50:01 -0800

    feature/bullet-list: Tweaks docs page

--- packages/hui-pages/src/routes/BulletListDocs/index.js
+++ packages/hui-pages/src/routes/BulletListDocs/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, Row } from 'react-flexbox-grid'
 
 import BulletList from '@hixme-ui/bullet-list'
 
@@ -15,11 +16,23 @@ const BulletListDocs = () => {
   return (
     <div>
       <DocsTitle title='BulletList' subtitle='A styled unordered list' />
-      <BulletList bulletArray={myUnorderedList} />
-      <br />
-      <Code>
-        {'<BulletList bulletArray={myUnorderedList} />'}
-      </Code>
+      <Grid fluid>
+        <Row center='xs' style={{ marginBottom: '30px' }}>
+          <BulletList bulletArray={myUnorderedList} />
+        </Row>
+        <Row center='xs'>
+          <Code>
+            {`const myUnorderedList = [
+  'One item',
+  'Two item',
+  'Red item',
+  'Blue item',
+]
+
+<BulletList bulletArray={myUnorderedList} />`}
+          </Code>
+        </Row>
+      </Grid>
     </div>
   )
 }
