bb29fbda7a8fecc7b093b651b6ee30f24f0e2dd0
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-18 15:26:44 -0800

    feature/forms: add forms package

---
+++ packages/forms/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import { Button, Input, Select, FormGroup, Checkbox } from '../src'
+
+describe('Forms', () => {
+  it('should define Button', () => {
+    expect(Button).toBeDefined()
+  })
+
+  it('should define Input', () => {
+    expect(Input).toBeDefined()
+  })
+
+  it('should define Select', () => {
+    expect(Select).toBeDefined()
+  })
+
+  it('should define Checkbox', () => {
+    expect(Checkbox).toBeDefined()
+  })
+
+  it('should define FormGroup', () => {
+    expect(FormGroup).toBeDefined()
+  })
+})
---
+++ packages/forms/src/index.js
@@ -0,0 +1,7 @@
+export Button from '@hixme-ui/button'
+export Checkbox from '@hixme-ui/checkbox'
+export Label from '@hixme-ui/label'
+export FormGroup from '@hixme-ui/form-group'
+export Input from '@hixme-ui/input'
+export Select from '@hixme-ui/select'
+
