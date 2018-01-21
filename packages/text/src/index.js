f5b99d9ec87888814f8efc978bf6bb7e0b8b18b1
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-20 23:06:13 -0800

    feature/radio-button: expose label base from text

--- packages/text/src/index.js
+++ packages/text/src/index.js
@@ -1,6 +1,7 @@
 import Text, {
   TextLink as _TextLink,
   Paragraph as _Paragraph,
+  LabelBase as _LabelBase,
   Label as _Label,
 } from './Text'
 import _TextBase from './TextBase'
@@ -9,5 +10,6 @@ export const TextBase = _TextBase
 export const TextLink = _TextLink
 export const Paragraph = _Paragraph
 export const Label = _Label
+export const LabelBase = _LabelBase
 
 export default Text
