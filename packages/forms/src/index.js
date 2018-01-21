3a3600b17ceddee21440ae9446f8a02fe2da7285
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-20 23:06:52 -0800

    feature/radio-button: expose RadioButton from Forms

--- packages/forms/src/index.js
+++ packages/forms/src/index.js
@@ -1,7 +1,7 @@
 export Button from '@hixme-ui/button'
 export Checkbox from '@hixme-ui/checkbox'
-export Label from '@hixme-ui/label'
 export FormGroup from '@hixme-ui/form-group'
 export Input from '@hixme-ui/input'
+export Label from '@hixme-ui/label'
+export RadioButton from '@hixme-ui/radio-button'
 export Select from '@hixme-ui/select'
-
