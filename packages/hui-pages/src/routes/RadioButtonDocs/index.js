19e92747d1f113af4303013980739200e2e1da1c
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-21 21:47:35 -0800

    feature/radio-button: reformat radio docs

--- packages/hui-pages/src/routes/RadioButtonDocs/index.js
+++ packages/hui-pages/src/routes/RadioButtonDocs/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Row } from 'react-flexbox-grid'
 
+import Container from '@hixme-ui/container'
 import RadioButton from '@hixme-ui/radio-button'
 import Title from '@hixme-ui/title'
 
@@ -13,14 +14,19 @@ const RadioDocs = () => (
       subtitle='A Hixme UI radio form component'
     />
     <Grid fluid>
-      <Row style={{ margin: '40px 0 20px 0' }}><Title smaller>Examples</Title></Row>
-      <Row center='xs' style={{ marginBottom: '20px' }}>
-        <RadioButton name='gender' text='Male' value='Male' />
-        <RadioButton name='gender' text='Female' value='Female' />
+      <Row style={{ marginBottom: '20px' }}>
+        <Container width='200px' margin='0 auto'>
+          <RadioButton name='gender' text='Male' value='Male' />
+          <br />
+          <br />
+          <RadioButton name='gender' text='Female' value='Female' />
+        </Container>
       </Row>
       <Row center='xs'>
         <Code>
           {`<RadioButton name='gender' text='Male' />
+<br />
+<br />
 <RadioButton name='gender' text='Female' />
 `}
         </Code>
