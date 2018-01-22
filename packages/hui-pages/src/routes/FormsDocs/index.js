7afe5e11b473cc4519ad96c670eb479c2fd7bb59
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-21 22:53:25 -0800

    feature/radio-button: update forms docs for radio button and select examples

--- packages/hui-pages/src/routes/FormsDocs/index.js
+++ packages/hui-pages/src/routes/FormsDocs/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Container from '@hixme-ui/container'
-import { Button, Input, FormGroup, Label } from '@hixme-ui/forms'
+import { Button, Input, FormGroup, Label, Select, RadioButton } from '@hixme-ui/forms'
 import { Code, DocsTitle } from '../../components'
 
 /* eslint-disable jsx-a11y/label-has-for */
@@ -12,23 +12,79 @@ const FormsDocs = () => (
       subtitle='Single import for HUI form components'
     />
     <Container width='500px' margin='0px auto'>
-      <br />
       <form>
+        <FormGroup>
+          <Label htmlFor='title'>Title</Label>
+          <Select name='k' id='title'>
+            <option value='mr'>Mr</option>
+            <option value='mrs'>Mrs</option>
+            <option value='ms'>Ms</option>
+            <option value='miss'>Miss</option>
+          </Select>
+        </FormGroup>
         <FormGroup>
           <Label htmlFor='firstname'>First name</Label>
           <Input name='firstname' id='firstname' />
+        </FormGroup>
+        <FormGroup>
+          <RadioButton
+            name='gender'
+            text='Male'
+            value='male'
+            style={{
+              display: 'inline-block',
+              marginRight: '30px',
+            }}
+          />
+          <RadioButton
+            name='gender'
+            text='Female'
+            value='female'
+          />
         </FormGroup>
         <Button>Submit</Button>
       </form>
     </Container>
     <Code>
-      {`import { Button, Input, FormGroup, Checkbox, Select, Label } from '@hixme-ui/forms'
-
+      {`import {
+  Button,
+  Checkbox,
+  FormGroup,
+  Input,
+  Label,
+  RadioButton,
+  Select,
+} from '@hixme-ui/forms'
 
 <form>
   <FormGroup>
+    <Label htmlFor='title'>Title</Label>
+    <Select name='k' id='title'>
+      <option value='mr'>Mr</option>
+      <option value='mrs'>Mrs</option>
+      <option value='ms'>Ms</option>
+      <option value='miss'>Miss</option>
+    </Select>
+  </FormGroup>
+  <FormGroup>
     <Label htmlFor='firstname'>First name</Label>
     <Input name='firstname' id='firstname' />
+  </FormGroup>
+  <FormGroup>
+    <RadioButton
+      name='gender'
+      text='Male'
+      value='male'
+      style={{
+        display: 'inline-block',
+        marginRight: '30px',
+      }}
+    />
+    <RadioButton
+      name='gender'
+      text='Female'
+      value='female'
+    />
   </FormGroup>
   <Button>Submit</Button>
 </form>
