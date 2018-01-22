d103d413b4d7fa5454956700804e7938390f971c
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-21 21:28:18 -0800

    feature/radio-button: clean up radio button

--- packages/radio-button/__tests__/index.test.js
+++ packages/radio-button/__tests__/index.test.js
@@ -3,11 +3,65 @@ import renderer from 'react-test-renderer'
 import { ThemeProvider } from '@hixme-ui/theme'
 import RadioButton from '../src'
 
-test('RadioButton', () => {
+describe('RadioButton', () => {
   const component = renderer.create(
-    (<ThemeProvider><RadioButton name='gender' text='Male' /></ThemeProvider>),
+    (<ThemeProvider><RadioButton name='gender' text='Male' value='male' /></ThemeProvider>),
   )
-
   const tree = component.toJSON()
-  expect(tree.type).toBe('label')
+
+  it('should have a label wrapper', function() {
+    expect(tree.type).toBe('label')
+  })
+
+  it('should have 3 children', function() {
+    expect(tree.children.length).toBe(3)
+  })
+
+  describe('Radio control', function() {
+    const RadioControl = tree.children[0]
+
+    it('should be an input', function() {
+      expect(RadioControl.type).toBe('input')
+    })
+
+    it('should have prop type radio ', function() {
+      expect(RadioControl.props.type).toBe('radio')
+    })
+
+    it('should have prop name gender', function() {
+      expect(RadioControl.props.name).toBe('gender')
+    })
+
+    it('should have prop value male', function() {
+      expect(RadioControl.props.value).toBe('male')
+    })
+
+    it('should have prop id format', function() {
+      expect(RadioControl.props.id).toBe('hui:rb-gender-Male-male')
+    })
+  })
+
+  describe('Fake radio display', function() {
+    const FakeRadio = tree.children[1]
+
+    it('should be a span', function() {
+      expect(FakeRadio.type).toBe('span')
+    })
+
+    it('should have prop className with fakeRadio', function() {
+      expect(FakeRadio.props.className.includes('fakeRadio')).toBe(true)
+    })
+  })
+
+  describe('Radio label display', function() {
+    const RadioLabel = tree.children[2]
+
+    it('should be a label', function() {
+      expect(RadioLabel.type).toBe('label')
+    })
+
+    it('should have child with text', function() {
+      expect(RadioLabel.children[0]).toBe('Male')
+    })
+  })
 })
--- packages/radio-button/src/RadioButton.js
+++ packages/radio-button/src/RadioButton.js
@@ -1,10 +1,84 @@
-/* eslint-disalbe */
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { LabelBase } from '@hixme-ui/text'
 
-const RadioButton = props => (
-  <label />
-)
+const RadioInput = styled.input`
+  position: fixed;
+    left: -9999px;
+    top: -9999px;
+    &:checked ~ {
+      .fakeRadio {
+        &:after {
+          opacity: 1;
+        }
+      }
+    }
+    &:focus ~ {
+      .fakeRadio {
+        outline: none;
+        border-color: #2fb699;
+        box-shadow: 0 0 10px #2fb699;
+      }
+    }
+`
+
+const RadioText = LabelBase.extend`
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 500;
+  font: 500 16px/18px;
+  color: #4a5b64;
+  margin: 0 0 0 16px;
+  max-width: calc(100% - 46px);
+  vertical-align: top;
+`
+
+const FakeRadio = styled.span`
+  cursor: pointer;
+  border-radius: 50%;
+  border: 1px solid #b9c5cd;
+  width: 18px;
+  height: 18px;
+  display: inline-block;
+  position: relative;
+  background: $white;
+  font-size: 0;
+  line-height: 0;
+  vertical-align: top;
+  &:after {
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    right: 2px;
+    bottom: 2px;
+    border-radius: 50%;
+    background: #2eae8f;
+    content: " ";
+    opacity: 0;
+    z-index: 2;
+    transition: all .1s ease-in-out;
+  }
+`
+export function radioId({
+  name = 'n',
+  text = 't',
+  value = 'v',
+}) {
+  return `hui:rb-${name.toString()}-${text.toString()}-${value.toString()}`
+}
+
+const RadioButton = (props) => {
+  const { text, name, ...restProps } = props
+
+  return (
+    <label htmlFor={radioId(props)}>
+      <RadioInput name={props.name} type='radio' {...restProps} id={radioId(props)} />
+      <FakeRadio className='fakeRadio' />
+      {props.text && <RadioText>{text}</RadioText>}
+    </label>
+  )
+}
 
 RadioButton.propTypes = {
   name: PropTypes.string.isRequired,
