ead5a218cfbcc3f7db4d6e46f5763b1fda566299
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-01-22 13:01:22 -0800

    feature/card-drawer: Remove console.log from ChevronRow

--- packages/card-drawer/src/ChevronRow.js
+++ packages/card-drawer/src/ChevronRow.js
@@ -2,10 +2,7 @@ import styled from 'styled-components'
 import { Row } from 'react-flexbox-grid'
 
 const ChevronRow = styled(Row)`
-  transform: ${(props) => {
-    console.log('props --- ', props)
-    return (props.open ? 'rotate(270deg)' : 'rotate(90deg)')
-  }};
+  transform: ${props => (props.open ? 'rotate(270deg)' : 'rotate(90deg)')};
   transition: transform .25s ease;
 `
 
