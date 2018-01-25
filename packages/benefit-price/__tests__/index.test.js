24b5ce8baba1b8492e53c6b630215b1837029ed6
ijoel <joel.iniguez@gmail.com>
2018-01-24 21:36:01 -0800

    improvement/ARQ-387: Add tests

--- packages/benefit-price/__tests__/index.test.js
+++ packages/benefit-price/__tests__/index.test.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { ThemeProvider } from '@hixme-ui/theme'
-import Component from '../src/index.js'
+import BenefitPrice from '../src/index'
+import 'jest-styled-components'
 
-test('Component', () => {
-  const component = renderer.create(
-    (<ThemeProvider><Component /></ThemeProvider>),
-  )
+describe('BenefitPrice', () => {
+  test('it renders', () => {
+    const benefitPrice = renderer.create(<ThemeProvider><BenefitPrice value={100} /></ThemeProvider>)
 
-  const tree = component.toJSON()
-  expect(tree.type).toBe('div')
+    const tree = benefitPrice.toJSON()
+    expect(tree.type).toBe('div')
+  })
+
+  test('it has 0px padding by default', () => {
+    const component = renderer.create(
+      <ThemeProvider><BenefitPrice value={4321} /></ThemeProvider>,
+    )
+
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('padding', '0')
+  })
+
+  test('it should render with background: none', () => {
+    const component = renderer.create(
+      <ThemeProvider><BenefitPrice value={4321} /></ThemeProvider>,
+    )
+
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('background', 'none')
+  })
+
+  test('it should render with display: flex', () => {
+    const component = renderer.create(
+      <ThemeProvider><BenefitPrice value={4321} /></ThemeProvider>,
+    )
+
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('display', 'flex')
+  })
 })
