49e0cf2a83f2952cf446c98d47240a93f0d41ddb
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-01-26 14:29:29 -0800

    feature/home-updates: Home route check in DocsHeader

--- packages/hui-pages/src/components/DocsHeader/DocsHeader.js
+++ packages/hui-pages/src/components/DocsHeader/DocsHeader.js
@@ -1,22 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 import { Link } from 'react-router-dom'
 
 import Text from '@hixme-ui/text'
 
-const DocsHeader = () => (
+const DocsHeader = ({ location }) => (
   <Grid fluid style={{ padding: '0' }}>
-    <Row style={{ margin: '20px 0 30px 0' }}>
-      <Col>
-        <Link to='/'>
-          <Text light cursor='pointer'>
-            {'< Back to Home'}
-          </Text>
-        </Link>
-      </Col>
-    </Row>
+    {location.pathname !== '/' &&
+      <Row style={{ margin: '20px 0 30px 0' }}>
+        <Col>
+          <Link to='/'>
+            <Text light cursor='pointer'>
+              {'< Back to Home'}
+            </Text>
+          </Link>
+        </Col>
+      </Row>
+    }
   </Grid>
 )
 
+DocsHeader.propTypes = {
+  location: PropTypes.object,
+}
+
+DocsHeader.defaultProps = {
+  location: {},
+}
+
 DocsHeader.displayName = 'DocsHeader'
-export default DocsHeader
+export default withRouter(DocsHeader)
