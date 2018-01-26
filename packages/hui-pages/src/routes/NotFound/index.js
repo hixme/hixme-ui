3624ec10a106a66bebe265e12242bd05a2212ffb
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-01-26 14:30:14 -0800

    feature/home-updates: Update 404 page

--- packages/hui-pages/src/routes/NotFound/index.js
+++ packages/hui-pages/src/routes/NotFound/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Container from '@hixme-ui/container'
+import Text from '@hixme-ui/text'
+import Title from '@hixme-ui/title'
 
 const NotFound = () => (
-  <div>
-    <h1>404 - Not Found</h1>
-  </div>
+  <Container flex direction='column' alignCenter>
+    <Title jumbo bold primary>404 - Not Found</Title>
+    <Text fontSize='80px'>:(</Text>
+    <Text p large thin>
+      Perhaps head back to the <Link to='/'><Text primary bold cursor='pointer'>home page</Text></Link>?
+    </Text>
+  </Container>
 )
 
 export default NotFound
