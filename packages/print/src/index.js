c00a4abda399bf3da254fb1af42de2ebbc04d367
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-27 22:30:33 -0800

    feature/print: use Text as wrapper for component. fix alignment

--- packages/print/src/index.js
+++ packages/print/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import Icon from '@hixme-ui/icon'
 import Text from '@hixme-ui/text'
+
+const PrintLabel = styled.span`
+  margin-right: 6px;
+  margin-top: 3px;
+  display: inline-block;
+  vertical-align: top;
+`
 
 const handleClick = (e) => {
   e.preventDefault()
@@ -13,12 +21,15 @@ const Print = ({
   showLabel,
   label,
 }) => (
-  <a href='javascript:void(0)' style={{ display: 'inline-block' }} onClick={handleClick}>
-    {showLabel &&
-      <Text muted style={{ margin: '0 0.5em 0 0', verticalAlign: 'bottom' }}>{label}</Text>}
+  <Text
+    cursor='pointer'
+    muted
+    onClick={handleClick}
+  >
+    {showLabel && <PrintLabel>{label}</PrintLabel>}
     {showIcon &&
       <Icon printer fontSize={22} muted style={{ verticalAlign: 'middle' }} />}
-  </a>
+  </Text>
 )
 
 Print.propTypes = {
