ce524e1df4545e327e6bcad6aa14ebdac0c59a46
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-31 22:16:52 -0800

    improvement/HCP-1491: more tests for FormGroup
    HCP-1491 #time 15m

--- packages/form-group/__tests__/index.test.js
+++ packages/form-group/__tests__/index.test.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import FormGroup from '../src/index.js'
+import { ThemeProvider } from '@hixme-ui/theme'
+import FormGroup from '../src/index'
 
-test(`${FormGroup.displayName}`, () => {
-  const component = renderer.create(
-    (<FormGroup><span>Hello</span></FormGroup>),
-  )
+describe(`${FormGroup.displayName}`, () => {
+  it('should render a div', () => {
+    const component = renderer.create(
+      (<FormGroup><span>Hello</span></FormGroup>),
+    )
 
-  const tree = component.toJSON()
-  expect(tree.type).toBe('div')
+    const tree = component.toJSON()
+    expect(tree.type).toBe('div')
+  })
+
+
+  describe('FormGroup props', () => {
+    const component = renderer.create(
+      <ThemeProvider>
+        <FormGroup label='Name' error='Name is required'>
+          <input />
+        </FormGroup>
+      </ThemeProvider>,
+    )
+
+    const tree = component.toJSON()
+    it('should render 3 children', () => {
+      expect(tree.children.length).toBe(3)
+    })
+
+    it('should render label as the first child', () => {
+      expect(tree.children[0].type).toBe('label')
+    })
+
+    it('should render input as the second child', () => {
+      expect(tree.children[1].type).toBe('input')
+    })
+
+    it('should render input as the third child', () => {
+      expect(tree.children[2].type).toBe('span')
+    })
+  })
 })
 
