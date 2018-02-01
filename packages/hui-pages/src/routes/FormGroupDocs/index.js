2981435a13347540b661d78d470dbf2fd1991ace
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-31 21:49:56 -0800

    improvement/HCP-1491: update hui form-group docs
    HCP-1491 #time 10m

--- packages/hui-pages/src/routes/FormGroupDocs/index.js
+++ packages/hui-pages/src/routes/FormGroupDocs/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 
+import Title from '@hixme-ui/title'
 import FormGroup from '@hixme-ui/form-group'
 import Input from '@hixme-ui/input'
 
@@ -12,17 +14,42 @@ const FormGroupDocs = () => (
       title='Form Group'
       subtitle='Gives proper spacing for a grouping of label, form fields, and error messages'
     />
-    <FormGroup>
-      <label>Name</label>
-      <Input name='name' />
-    </FormGroup>
-    <Code>
-      {`<FormGroup>
+    <Grid fluid>
+      <Row style={{ paddingBottom: '10px' }}>
+        <Title small>Examples</Title>
+      </Row>
+      <Row>
+        <Col xs={4}>
+          <FormGroup>
+            <label>Name</label>
+            <Input name='name' />
+          </FormGroup>
+        </Col>
+        <Col xs={8}>
+          <Code>
+            {`<FormGroup>
   <label>Name</label>
   <Input name='name' />
-</FormGroup>
-`}
-    </Code>
+ </FormGroup>`}
+          </Code>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={4}>
+          <FormGroup label='Name' error='Name is required'>
+            <Input name='name' error />
+          </FormGroup>
+        </Col>
+        <Col xs={8}>
+          <Code>
+            {`<FormGroup>
+  <label>Name</label>
+  <Input name='name' />
+ </FormGroup>`}
+          </Code>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 
