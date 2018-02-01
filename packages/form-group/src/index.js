ea7a5a5172522b7f1e3eaa3123caa75a8cf173b6
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-31 21:49:26 -0800

    improvement/HCP-1491: add form group shortcuts for label and error
    HCP-1491 #time 45m

--- packages/form-group/src/index.js
+++ packages/form-group/src/index.js
@@ -1,6 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import Text from '@hixme-ui/text'
+import Label from '@hixme-ui/label'
 
-const FormGroup = styled.div`
+export const FormGroupBase = styled.div`
   padding: 13px 0px;
   > label {
     display: inline-block;
@@ -12,6 +16,28 @@ const FormGroup = styled.div`
     width: 100%;
   }
 `
+const FormGroup = ({
+  children,
+  label,
+  error,
+}) => (
+  <FormGroupBase>
+    {label && <Label>{label}</Label>}
+    {children}
+    {error && <Text error>{error}</Text>}
+  </FormGroupBase>
+)
+
+FormGroup.propTypes = {
+  children: PropTypes.node.isRequired,
+  label: PropTypes.string,
+  error: PropTypes.string,
+}
+
+FormGroup.defaultProps = {
+  label: null,
+  error: null,
+}
 
 FormGroup.displayName = 'hui:FormGroup'
 FormGroup.huiName = 'FormGroup'
