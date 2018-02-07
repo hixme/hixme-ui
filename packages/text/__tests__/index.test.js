1d4451b95e8ecd1d4a7d6bd6815aa18a0b00e8ec
Billy Montgomery <billy.montgomery@gmail.com>
2018-02-07 10:36:00 -0800

    improvement/expose-hyperlink: add some tests to named exports

--- packages/text/__tests__/index.test.js
+++ packages/text/__tests__/index.test.js
@@ -1,14 +1,53 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { ThemeProvider } from '@hixme-ui/theme'
-import Text from '../src'
+import Text, {
+  HyperLink,
+  Label,
+  LabelBase,
+  Paragraph,
+  TextBase,
+  TextLink,
+} from '../src'
 
-test('Input', () => {
-  const component = renderer.create(
-    (<ThemeProvider><Text>I said good day sir</Text></ThemeProvider>),
-  )
+describe('Input', () => {
+  it('should render a span', () => {
+    const component = renderer.create(
+      (<ThemeProvider><Text>I said good day sir</Text></ThemeProvider>),
+    )
 
-  const tree = component.toJSON()
-  expect(tree.type).toBe('span')
+    const tree = component.toJSON()
+    expect(tree.type).toBe('span')
+  })
+
+  describe('HyperLink', () => {
+    it('should be defined', () => {
+      expect(HyperLink).toBeDefined()
+    })
+  })
+
+  describe('TextLink', () => {
+    it('should be defined', () => {
+      expect(TextLink).toBeDefined()
+    })
+  })
+
+  describe('Paragraph', () => {
+    it('should be defined', () => {
+      expect(Paragraph).toBeDefined()
+    })
+  })
+
+  describe('Label', () => {
+    it('should be defined', () => {
+      expect(Label).toBeDefined()
+    })
+  })
+
+  describe('LabelBase', () => {
+    it('should be defined', () => {
+      expect(LabelBase).toBeDefined()
+    })
+  })
 })
 
