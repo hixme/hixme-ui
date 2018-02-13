418b589d682e2558ea91dfded0d7dfc704021068
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-02-13 14:22:40 -0800

    fix/card-drawer-layout-tweaks: Simplify layout in collapsible version of card-drawer

--- packages/card-drawer/src/ChevronRow.js
+++ packages/card-drawer/src/ChevronRow.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const ChevronRow = styled.div`
   transform: ${props => (props.open ? 'rotate(270deg)' : 'rotate(90deg)')};
   transition: transform .25s ease;
+  width: 20px;
 `
 
 ChevronRow.displayName = 'ChevronRow'
