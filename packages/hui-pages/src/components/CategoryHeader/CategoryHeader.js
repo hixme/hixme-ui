c84cbfd8789c594d1163f118265c74b95eab6eaa
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-02-19 09:21:33 -0800

    feature/split-button: Tweak margins on CategoryHeader

--- packages/hui-pages/src/components/CategoryHeader/CategoryHeader.js
+++ packages/hui-pages/src/components/CategoryHeader/CategoryHeader.js
@@ -6,7 +6,7 @@ import Separator from '@hixme-ui/separator'
 import Title from '@hixme-ui/title'
 
 const CategoryHeader = ({ title }) => (
-  <Container noPadding margin='40px 0 0 0'>
+  <Container noPadding margin='50px 0 20px 0'>
     <Title small>{title}</Title>
     <Separator size='1px' />
   </Container>
