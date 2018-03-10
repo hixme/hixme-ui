71190760dc9f18f0d1e5e79feb05672d2236d9e7
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-03-10 06:47:45 -0800

    feature/table-spacing: Update Table docs

--- packages/hui-pages/src/routes/TableDocs/index.js
+++ packages/hui-pages/src/routes/TableDocs/index.js
@@ -4,7 +4,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid'
 import Table from '@hixme-ui/table'
 import Title from '@hixme-ui/title'
 
-import DocsTitle from '../../components/DocsTitle'
+import { Code, DocsTitle } from 'components'
 
 const CheckboxDocs = () => (
   <Grid fluid>
@@ -12,6 +12,28 @@ const CheckboxDocs = () => (
       title='Table'
       subtitle='A styled table component. Highlights every other row when receiving the striped prop'
     />
+    <Row>
+      <Col xs={12}>
+        <Code>
+          {`import Table from '@hixme-ui/table'
+          
+<Table striped alignHeader='left' alignBody='right'>
+  <thead>
+    <tr>
+      <th>Header</th>
+      <th>Header</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Table data</td>
+      <td>Table data</td>
+    </tr>
+  </tbody>
+</Table>`}
+        </Code>
+      </Col>
+    </Row>
     <Row center='xs' style={{ marginBottom: '20px' }}>
       <Col xs={12} sm={8}>
         <Row style={{ margin: '20px 0' }}>
@@ -83,6 +105,41 @@ const CheckboxDocs = () => (
               <td>Table data</td>
               <td>Table data</td>
               <td>Table data</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+    <Row style={{ margin: '40px 0 20px 0' }}><Title smaller>Props</Title></Row>
+    <Row center='xs'>
+      <Col xs={12}>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Default</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>striped</td>
+              <td>boolean</td>
+              <td>false</td>
+              <td>Different background color on alternating rows</td>
+            </tr>
+            <tr>
+              <td>alignHeader</td>
+              <td>string</td>
+              <td>{"'center'"}</td>
+              <td>Text align on the {'<thead />'}</td>
+            </tr>
+            <tr>
+              <td>alignBody</td>
+              <td>string</td>
+              <td>{"'left'"}</td>
+              <td>Text align on the {'<tbody />'}</td>
             </tr>
           </tbody>
         </Table>
