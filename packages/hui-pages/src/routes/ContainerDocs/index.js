22fb51170fc6adcbeba9754834c7847b2775c0ec
Billy Montgomery <billy.montgomery@gmail.com>
2018-03-29 09:02:20 -0700

    improvement/container-opacity: sort container base props

--- packages/hui-pages/src/routes/ContainerDocs/index.js
+++ packages/hui-pages/src/routes/ContainerDocs/index.js
@@ -1,21 +1,50 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 
 import Container from '@hixme-ui/container'
 import Text from '@hixme-ui/text'
 
 import { Code, DocsTitle } from '../../components'
 
+const rowExampleStyles = { paddingBottom: '15px' }
+
 const ContainerDocs = () => (
   <div>
-    <DocsTitle title='Container' subtitle='A container component offering layout and styling options' />
-    <Container border='1px solid red' muted rounded margin='0 0 30px 0'>
-      <Text>I am in a container</Text>
-    </Container>
-    <Code>
-      {`<Container border='1px solid red' muted rounded margin='0 0 30px 0'>
+
+    <DocsTitle
+      title='Container'
+      subtitle='A container component offering layout and styling options'
+    />
+    <Grid fluid>
+      <Row style={rowExampleStyles}>
+        <Col xs={4}>
+          <Container border='1px solid red' muted rounded margin='0 0 30px 0'>
+            <Text>I am in a container</Text>
+          </Container>
+        </Col>
+        <Col xs={8}>
+          <Code>
+            {`<Container border='1px solid red' muted rounded margin='0 0 30px 0'>
   <Text>I am in a container</Text>
 </Container>`}
-    </Code>
+          </Code>
+        </Col>
+      </Row>
+      <Row style={rowExampleStyles}>
+        <Col xs={4}>
+          <Container opacity='.6' primary margin='0 0 30px 0'>
+            <Text>I am in a container with opacity</Text>
+          </Container>
+        </Col>
+        <Col xs={8}>
+          <Code>
+            {`<Container opacity='.6' primary margin='0 0 30px 0'>
+  <Text>I am in a container with opacity</Text>
+</Container>`}
+          </Code>
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 
