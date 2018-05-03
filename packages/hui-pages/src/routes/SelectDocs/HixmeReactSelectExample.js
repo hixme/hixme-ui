edbd939785d3d579417d99af8c4222896f85070f
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-05-03 16:38:35 -0700

    feature/hixme-react-select: Add example

---
+++ packages/hui-pages/src/routes/SelectDocs/HixmeReactSelectExample.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { HixmeReactSelect } from '@hixme-ui/select'
+
+class HixmeReactSelectExample extends Component {
+  static propTypes = {
+    value: PropTypes.string,
+  }
+
+  static defaultProps = {
+    value: '',
+    onChange: () => null,
+  }
+
+  state = { value: this.props.value }
+  handleOnChange = (value) => {
+    this.setState({ value })
+  }
+  render() {
+    const { value } = this.state
+    return (
+      <HixmeReactSelect
+        onChange={this.handleOnChange}
+        value={value}
+        options={[
+          { value: 1, label: 'You' },
+          { value: 2, label: 'were' },
+          { value: 3, label: 'my' },
+          { value: 4, label: 'brother' },
+          { value: 5, label: 'Anakin' },
+        ]}
+      />
+    )
+  }
+}
+
+export default HixmeReactSelectExample
--- packages/hui-pages/src/routes/SelectDocs/index.js
+++ packages/hui-pages/src/routes/SelectDocs/index.js
@@ -6,14 +6,12 @@ import Table from '@hixme-ui/table'
 import Text from '@hixme-ui/text'
 import Title from '@hixme-ui/title'
 
+import HixmeReactSelectExample from './HixmeReactSelectExample'
 import { Code, DocsTitle } from '../../components'
 
 const SelectDocs = () => (
   <div>
-    <DocsTitle
-      title='Select'
-      subtitle='A Hixme UI styled select component'
-    />
+    <DocsTitle title='Select' subtitle='A Hixme UI styled select component' />
     <Grid fluid>
       <Row style={{ marginBottom: '15px' }}>
         <Col xs={12}>
@@ -22,7 +20,9 @@ const SelectDocs = () => (
       </Row>
       <Row>
         <Col xs={12} sm={6}>
-          <Select list={[{ id: 0, name: 'Hello' }, { id: 1, name: 'Goodbye' }]} />
+          <Select
+            list={[{ id: 0, name: 'Hello' }, { id: 1, name: 'Goodbye' }]}
+          />
           <Code>
             {`<Select 
   list={[
@@ -53,12 +53,17 @@ const SelectDocs = () => (
       <Row>
         <Col xs={12}>
           <Select loading list={[{ id: 0, name: 'Hello' }]} />
-          <Code>
-            {'<Select loading list={[{ id: 0, name: \'Hello\' }]} />'}
-          </Code>
+          <Code>{"<Select loading list={[{ id: 0, name: 'Hello' }]} />"}</Code>
         </Col>
       </Row>
-      <Row style={{ margin: '40px 0 20px 0' }}><Title smaller>Props</Title></Row>
+      <Row>
+        <Col xs={12}>
+          <HixmeReactSelectExample />
+        </Col>
+      </Row>
+      <Row style={{ margin: '40px 0 20px 0' }}>
+        <Title smaller>Props</Title>
+      </Row>
       <Row center='xs'>
         <Col xs={12}>
           <Table striped>
@@ -75,13 +80,19 @@ const SelectDocs = () => (
                 <td>list</td>
                 <td>array</td>
                 <td>N/A</td>
-                <td>Array populated with ID and Name for each option on the Select component</td>
+                <td>
+                  Array populated with ID and Name for each option on the Select
+                  component
+                </td>
               </tr>
               <tr>
                 <td>placeholder</td>
                 <td>string</td>
                 <td>null</td>
-                <td>Select placeholder text. Placeholder option has value of empty string</td>
+                <td>
+                  Select placeholder text. Placeholder option has value of empty
+                  string
+                </td>
               </tr>
               <tr>
                 <td>loading</td>
