858651b465ae86a3be71f4ab2e7592f629119d09
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-06-05 14:34:42 -0700

    feature/toggle-cc: Update docs
    toggle-cc #time 10m

--- packages/hui-pages/src/routes/ToggleDocs/index.js
+++ packages/hui-pages/src/routes/ToggleDocs/index.js
@@ -1,25 +1,94 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 
+import Table from '@hixme-ui/table'
 import Toggle from '@hixme-ui/toggle'
-// import Title from '@hixme-ui/title'
+import Title from '@hixme-ui/title'
 
-import { DocsTitle } from '../../components'
+import { Code, DocsTitle } from '../../components'
 
-const CheckboxDocs = () => (
-  <Grid fluid>
-    <DocsTitle
-      title='Toggle'
-      subtitle='A hixme-ui styled toggle component'
-    />
-    <Row center='xs' style={{ marginBottom: '20px' }}>
-      <Col xs={12} sm={8}>
-        <Row style={{ margin: '20px 0' }}>
-          <Toggle />
+class ToggleDocs extends React.Component {
+  state = { checked: false }
+
+  handleToggle(e) {
+    this.setState({ checked: e.target.checked })
+  }
+
+  render() {
+    return (
+      <Grid fluid>
+        <DocsTitle
+          title='Toggle'
+          subtitle='A hixme-ui styled toggle component'
+        />
+        <Row center='xs' style={{ marginBottom: '20px' }}>
+          <Col xs={12}>
+            <Toggle
+              checked={this.state.checked}
+              truthyText='Ya'
+              falsyText='Nah'
+              onChange={e => this.handleToggle(e)}
+            />
+          </Col>
         </Row>
-      </Col>
-    </Row>
-  </Grid>
-)
+        <Row>
+          <Col xs={12}>
+            <Code>
+              {`import Toggle from '@hixme-ui/toggle'
 
-export default CheckboxDocs
+<Toggle
+  checked={this.state.checked}
+  truthyText='Ya'
+  falsyText='Nah'
+  onChange={e => this.handleToggle(e)}
+/>`}
+            </Code>
+          </Col>
+        </Row>
+        <Row style={{ margin: '40px 0 20px 0' }}><Title smaller>Props</Title></Row>
+        <Row center='xs'>
+          <Col xs={12}>
+            <Table striped>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Default</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>checked</td>
+                  <td>boolean</td>
+                  <td>undefined</td>
+                  <td>Controls toggle state</td>
+                </tr>
+                <tr>
+                  <td>truthyText</td>
+                  <td>string</td>
+                  <td>&apos;Yes&apos;</td>
+                  <td>Label shown when toggle is true</td>
+                </tr>
+                <tr>
+                  <td>falsyText</td>
+                  <td>string</td>
+                  <td>&apos;No&apos;</td>
+                  <td>Label shown when toggle is false</td>
+                </tr>
+                <tr>
+                  <td>noLabels</td>
+                  <td>boolean</td>
+                  <td>false</td>
+                  <td>Hide labels</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+
+export default ToggleDocs
