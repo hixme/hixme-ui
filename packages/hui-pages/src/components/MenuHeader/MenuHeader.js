c58caf557d5ec9000bf7db2b8061c5b2860082fc
willthebrit <will.morgan@hixme.com>
2018-06-28 10:23:24 -0700

    enhancement/fonts-buttons: Add Home link to MenuHeader

--- packages/hui-pages/src/components/MenuHeader/MenuHeader.js
+++ packages/hui-pages/src/components/MenuHeader/MenuHeader.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import Container from '@hixme-ui/container'
+import Title from '@hixme-ui/title'
 
 const MenuHeader = () => (
-  <Container flex justifyEnd alignCenter noPadding width='100%'>
+  <Container flex justifySpaceBetween alignCenter noPadding width='100%'>
+    <Link to='/'>
+      <Title
+        smaller
+        cursor='pointer'
+        secondary
+        style={{ marginLeft: '20px' }}
+      >
+        Home
+      </Title>
+    </Link>
     <a className='github-button' href='https://github.com/hixme/hixme-ui' data-size='large' aria-label='Star hixme/hixme-ui on GitHub'>Star</a>
   </Container>
 )
